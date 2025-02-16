2ec1d8eb402ffe64859487133bf649bf780fd7b5
tansxerd1231 <tansxerd1231@gmail.com>
2025-02-17 00:12:41 +0700

    add create products collection

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,31 @@
 import express from "express"
 import dotenv from "dotenv"
 import { connectDB } from './config/db.js'
+import Product from "./models/product.model.js";
+
 dotenv.config();
 
 const app = express();
 
-app.get('/products', (req,res)=> {
-    res.send("server is ready 1213");
-})
+app.use(express.json());//allow us to accept JSON data in the body
 
+app.post("/api/products", async(req,res)=> {
+    const product = req.body;//user will send this data
+    if(!product.name || !product.price || !product.image){
+        return res.status(400).json({success:false, message:"please provide all field"});
+    }
+    const newProduct = new Product(product)
+
+    try {
+        await newProduct.save();
+        res.status(201).json({success:true , data : newProduct});
+    }
+    catch (error)
+    {
+        console.error("Error create product on database",error.message);
+        res.status(500).json({success:false ,message :"server Error"});
+    }
+});
 console.log(process.env.MONGO_URI);
 
 app.listen(5000,() => {
